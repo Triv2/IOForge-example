159f32d9f8bb633bf1f25f582c49e80c807c8b05
Trivial-lore <triviallore@gmail.com>
2023-07-17 08:41:01 -0500

    setting up SEO, Header, basic packages, and layout

---
+++ src/config/settings.js
@@ -0,0 +1,7 @@
+export default {
+	title: `IO Forge | A Software as a Service Business`,
+	description: `Software solutions to your business needs.`,
+	url: `https://ioforge.com`, // No trailing slash!
+	name: `IO Forge`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: true,
+};
